ef950c9e608a3751b4cc436b04e9876c530f8cb9
hinsjain <hinsjain8983@gmail.com>
2021-11-30 19:47:42 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,52 @@
 const express = require("express");
 const db = require("../config.js");
-	router = express.Router();
+router = express.Router();
 
 // get user lists
 router.get("/list", function (req, res) {
 	let sql = `SELECT * FROM users`;
 	db.query(sql, function (err, data, fields) {
 		if (err) throw err;
-		res.json({data});
+		res.json({ data });
 	});
 });
 
-router.post('/new', function(req, res) {
-    let sql = `INSERT INTO users(full_name, qr_code, created) VALUES (?)`;
-    let values = [
-      req.body.full_name,
-      req.body.qr_code,
-      req.body.created
-    ];
-    db.query(sql, [values], function(err, data, fields) {
-      if (err) throw err;
-      res.json({
-        status: 200,
-        message: "New user added successfully"
-      })
-    })
+router.post("/new", function (req, res) {
+	let sql = `INSERT INTO users(full_name, qr_code, created) VALUES (?)`;
+	let values = [req.body.full_name, req.body.qr_code, req.body.created];
+	db.query(sql, [values], function (err, data, fields) {
+		if (err) throw err;
+		res.json({
+			status: 200,
+			message: "New user added successfully",
+		});
+	});
 });
 
-router.post('/tracker', function(req, res) {
-    let sql = `INSERT INTO tracker_list(full_name, verified) VALUES (?)`;
-    let values = [
-      req.body.full_name,
-      req.body.verified
-    ];
-    db.query(sql, [values], function(err, data, fields) {
-      if (err) throw err;
-      res.json({
-        status: 200,
-        message: "Entry added successfully"
-      })
-    })
+router.get("/tracker_list", function (req, res) {
+	let sql = `SELECT * FROM tracker_list`;
+	db.query(sql, function (err, data, fields) {
+		if (err) throw err;
+		res.json({ data });
+	});
+});
+
+router.post("/tracker_new", function (req, res) {
+	let find = `SELECT * FROM tracker_list WHERE user_id = ${req.body.user_id}`;
+	db.query(find, function (err, result, fields) {
+        
+		if (result.length < 1) {
+			let sql = `INSERT INTO tracker_list(user_id, verified) VALUES (?)`;
+			let values = [req.body.user_id, req.body.verified];
+			db.query(sql, [values], function (err, data, fields) {
+				if (err) throw err;
+				res.json({
+					status: 200,
+					message: "Entry added successfully",
+				});
+			});
+		}
+	});
 });
 
 module.exports = router;
